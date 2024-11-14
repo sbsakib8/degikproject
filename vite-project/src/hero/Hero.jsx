a75c949c537b08e19bbce45b296e80e2d4e99698
sakib <sbsakibsarkar8@gmail.com>
2024-11-15 02:47:20 +0600

    hero add

--- vite-project/src/hero/Hero.jsx
+++ vite-project/src/hero/Hero.jsx
@@ -5,23 +5,23 @@ function Hero() {
   return (
     <>
     {/* main div */}
-        <div className=' flex flex-1 flex-col lg:flex-row container'>
+        <div className=' flex flex-1 flex-col lg:flex-row container mt-2'>
             {/* left sight */}
-          <div>
-            <h1  className=' max-w-[642] h-174px mt-5 mr-5 '> Simply explained with illustrations</h1>
-            <p>There are a lot of different components that will help you create the perfect look for your project</p>
+          <div className=' order-2 lg:order-1 '>
+            <h1  className='max-w-[682px]  font-bold text-[50px] lg:text-[72px] lg:leading-[87.14px] mb-2 text-wrap '> Simply explained with illustrations</h1>
+            <p className=' font-normal text-[21px] leading-8 max-w-[448px] text-wrap'>There are a lot of different components that will help you create the perfect look for your project</p>
             {/* button div */}
-            <div>
-               <Button btn='Get Started'/>
+            <div className='flex gap-3 mt-3'>
+               <Button  btn='Get Started'/>
                <Button btn='Learn More'/>
             </div>
           </div>
 
 
           {/* right sight */}
-          <div>
+          <div className=' order-1 lg:order-2 '>
 
-            <img src="/Component 1.png" alt="" />
+            <img className=' w-full flex-1' src="/Component 1.png" alt="" />
           </div>
         </div>
     </>
