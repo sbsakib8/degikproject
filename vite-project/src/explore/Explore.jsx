0b33fdce4b798e02edd739b156b062a857836b33
sakib <sbsakibsarkar8@gmail.com>
2024-11-16 01:18:41 +0600

    data file add

--- vite-project/src/explore/Explore.jsx
+++ vite-project/src/explore/Explore.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Button from '../compronent/button/Button'
 
 function Explore() {
   return (
-    <div>Explore</div>
+    <>
+  {/* main div */}
+       <div>
+        {/* left sight */}
+        <div>
+           
+        </div>
+      
+        {/* right sight */}
+         <div>
+          <h2>The quickest way to create modern presentation</h2>
+          <p>Best software platform for running an internet business. We build the most powerful and flexible tools for internet commerce.</p>
+           <Button btn='Explore All'/>
+         </div>
+       
+
+       </div>
+
+
+    </>
   )
 }
 
--- vite-project/src/quickest/Quickest.jsx
+++ vite-project/src/quickest/Quickest.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 function Quickest() {
   return (
-    <div>Quickest</div>
+    <div>Quickest
+    </div>
   )
 }
 
