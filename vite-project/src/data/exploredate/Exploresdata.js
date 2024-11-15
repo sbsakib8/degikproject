e82023535813dacc098af90622259467af3f3d14
sakib <sbsakibsarkar8@gmail.com>
2024-11-16 02:20:21 +0600

    cord add

---
+++ vite-project/src/data/exploredate/Exploresdata.js
@@ -0,0 +1,28 @@
+let Exploresdata= [
+    {
+        id: 1,
+        ing: 'floppy 1.png',
+        title: 'Managment',
+        preragraph: "Software platform for running your new internet business",
+    },
+     {
+        id: 2,
+        ing: 'floppy 1 (3).png',
+        title: 'Entertainment',
+        preragraph: "Software platform for running your new internet business",
+    },
+
+    {
+        id: 3,
+        ing: 'floppy 1 (4).png',
+        title: 'Marketing',
+        preragraph: "Software platform for running your new internet business",
+    },
+    {
+        id: 4,
+        ing: 'floppy 1 (1).png',
+        title: 'References',
+        preragraph: "Software platform for running your new internet business",
+    },
+]
+export default Exploresdata;
--- vite-project/src/explore/Explore.jsx
+++ vite-project/src/explore/Explore.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import Button from '../compronent/button/Button'
-
+import Exploresdata from '../data/exploredate/Exploresdata'
 function Explore() {
   return (
     <>
   {/* main div */}
-       <div>
+       <div className=' container bg-boxColor grid grid-cols-1 md:grid-cols-2 gap-5'>
         {/* left sight */}
-        <div>
-           
+        <div className='grid grid-cols-1 mx-auto  md:grid-cols-2 gap-2 md:gap-x-32 lg:gap-x-40'>
+        {
+          Exploresdata.map((item) => (
+            <div  key={item.id} className='  bg-white w-[300px] h-[310] flex flex-1 flex-col rounded-xl gap-7 '>
+             <div className='bg-boxColor w-[130px] h-[130px] rounded-full'>
+             <img className=' mx-auto mt-3 w-[96px] h-[96px]' src={item.ing} alt={item.title} />
+             </div>
+              
+                <h3 className=' font-medium text-[24px] leading-7 text-center'>{item.title}</h3>
+                <p className=' max-w-[236px] text-[17px] font-normal leading-6'>{item.preragraph}</p>
+             
+            </div>
+          ))
+        }
         </div>
       
         {/* right sight */}
@@ -24,6 +36,6 @@ function Explore() {
 
     </>
   )
-}
+  }
 
 export default Explore
