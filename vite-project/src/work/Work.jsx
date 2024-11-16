81c27f200d743b39805cc9ba5fe08073e9020635
sakib <sbsakibsarkar8@gmail.com>
2024-11-17 02:41:21 +0600

    work done

--- vite-project/src/work/Work.jsx
+++ vite-project/src/work/Work.jsx
@@ -5,28 +5,28 @@ function Work() {
   return (
     <>
     {/* main div */}
-      <div className=' container flex flex-1 gap-4 flex-col lg:flex-row'>
+      <div className=' container flex flex-1 gap-4 flex-col lg:flex-row py-8 md:py-12 lg:py-16'>
           {/* left div */}
-        <div className='w-1/2'>
+        <div className=' lg:w-1/2'>
 
           {/* logo div */}
           <div>
-            <img className='  text-white w-[]' src="/Shape.png" alt="" />
-            <p>Logo</p>
+            <img className=' text-white w-[34px] h-[34px] inline-block' src="/Shape.png" alt="" />
+            <p className=' inline-block ml-5 text-[20px] font-medium'>Logo</p>
           </div>
 
-          <h1>Make more time for the work that matters most</h1> 
-          <p>Best software platform for running an internet business.</p>
+          <h1 className=' lg:max-w-[463px] text-wrap text-[55px] font-medium  leading-[55px] lg:leading-[60px] mt-4'>Make more time for the work that matters most</h1> 
+          <p className=' max-w-[322px] my-4 text-[21px] font-normal leading-[30px] text-wrap text-gray-500 '>Best software platform for running an internet business.</p>
 
           <div>
-            <button> <ArrowBackIosIcon/> </button>
-            <button> <ArrowForwardIosIcon/> </button>
+            <button className=' bg-gray-300 w-[50px] h-[50px] rounded-full pl-2 mr-6'> <ArrowBackIosIcon/> </button>
+            <button className=' bg-gray-300 w-[50px] h-[50px] rounded-full pl-2'> <ArrowForwardIosIcon/> </button>
           </div>
         </div>
         {/* right div */}
         <div>
 
-          <img src="/Group 35.png" alt="" />
+          <img className=' my-5 mx-auto h-[405px] lg:max-w-[471px] lg:h-[551px] object-cover' src="/Group 35.png" alt="" />
         </div>
 
       </div>
