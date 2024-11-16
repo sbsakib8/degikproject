3bc5fda4d8c27165eaf70615f307d82c843bd701
sakib <sbsakibsarkar8@gmail.com>
2024-11-17 03:05:25 +0600

    design

--- vite-project/src/designss/Design.jsx
+++ vite-project/src/designss/Design.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Design() {
   return (
-    <div>Design</div>
+    <div className=' bg-boxColor'>
+       
+       <div className=' container py-10'>
+       <h1 className=' text-[30px] md:text-[55px] font-medium leading-[60px] text-center mb-3 lg:mb-10 '>Use illustrations in UI design</h1>
+       <img src="Mask Group.png" alt="" />
+       </div>
+       
+
+    </div>
   )
 }
 
