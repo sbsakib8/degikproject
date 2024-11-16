e535bfa0fa237b5162840a1c960f4ac9eb9f7e77
sakib <sbsakibsarkar8@gmail.com>
2024-11-17 02:16:38 +0600

    work add

--- vite-project/src/work/Work.jsx
+++ vite-project/src/work/Work.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
-
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 function Work() {
   return (
     <>
     {/* main div */}
-      <div>
+      <div className=' container flex flex-1 gap-4 flex-col lg:flex-row'>
           {/* left div */}
-        <div></div>
+        <div className='w-1/2'>
+
+          {/* logo div */}
+          <div>
+            <img className='  text-white w-[]' src="/Shape.png" alt="" />
+            <p>Logo</p>
+          </div>
+
+          <h1>Make more time for the work that matters most</h1> 
+          <p>Best software platform for running an internet business.</p>
+
+          <div>
+            <button> <ArrowBackIosIcon/> </button>
+            <button> <ArrowForwardIosIcon/> </button>
+          </div>
+        </div>
         {/* right div */}
-        <div></div>
+        <div>
+
+          <img src="/Group 35.png" alt="" />
+        </div>
 
       </div>
 
