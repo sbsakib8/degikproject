df292f8276e3767e32c37a9b9812f9beeec3ac51
sakib <sbsakibsarkar8@gmail.com>
2024-11-19 01:58:45 +0600

    quickest

--- vite-project/src/compronent/button/Button.jsx
+++ vite-project/src/compronent/button/Button.jsx
@@ -4,7 +4,7 @@ function Button(props) {
   return (
     <div>
         
-        <button className=' text-white w-[121px] h-[49px] rounded-xl py-[14px] bg-[#5F62E2]'>{props.btn}</button>
+        <button className= ' text-white w-[121px] h-[49px] rounded-xl py-[14px] bg-[#5F62E2]'>{props.btn}</button>
     </div>
   )
 }
--- vite-project/src/quickest/Quickest.jsx
+++ vite-project/src/quickest/Quickest.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
+
 function Quickest() {
   return (
-    <div>Quickest
+    <div className=' bg-primaryColor w-full  h-[345px]'>
+
+    <div className=' container flex flex-col md:flex-row flex-1 justify-between'>
+      {/* left div */}
+      <div className=' mt-4 md:mt-9 lg:mt-20'>
+        <h1 className=' font-medium text-[55px] leading-[60px] text-wrap text-white'>The quickest way to create awesome presentation</h1>
+        {/*button div*/}
+        <div className=' flex flex-1 gap-4 mt-5'>
+        <button className= ' text-white w-[121px] h-[49px] rounded-xl py-[14px] bg-[#37ea52]'>Get Started</button>
+        <button className= ' text-white w-[121px] h-[49px] rounded-xl py-[14px] bg-[#7071b7]'>Learn More</button>
+          
+        </div>
+      </div>
+
+      {/* right div */}
+      <div className=''>
+        <img className=' w-56 md:w-full' src="bomb 1.png" alt="" />
+      </div>
+
+    </div>
+
+
     </div>
   )
 }
