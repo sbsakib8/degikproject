096349c33026b6193dc96745bc6cb7691ee507b9
sakib <sbsakibsarkar8@gmail.com>
2024-11-19 02:52:28 +0600

    blog done

--- vite-project/src/blog/Blog.jsx
+++ vite-project/src/blog/Blog.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
-
+import Blogdata from './../data/blog/Blogdata';
+import Button from '../compronent/button/Button';
 function Blog() {
   return (
-    <div>Blog</div>
+    <div className=' container py-8 md:py-11 lg:py-16 '>
+      {/* top div */}
+      <div className=' flex flex-1 flex-col lg:flex-row lg:justify-between '>
+        {/* title div */}
+        <div>
+          <h1 className=' font-medium text-[55px] leading-[60px] text-wrap text-black'>Get more from our blog</h1>
+          <p className=' max-w-[448px] text-[21px] leading-[30px] text-wrap text-gray-400 '>There are a lot of different components that will help you create the perfect look for your project.</p>
+        </div>
+        {/* button dib */}
+        <div>
+          <Button btn='Explore All'/>
+        </div>
+      </div>
+
+{/* cord div */}
+      <div >
+        <div className=' grid md:grid-cols-2 lg:grid-cols-3 lg:gap-6 mt-10 '>
+         {
+          Blogdata.map((item) => (
+            <div className=' mt-3 w-[410px] h-[471px] mx-auto'>
+             <div className=' w-[350px] h-[275px] bg-boxColor rounded-[10px] '> <img className=' mx-auto pt-9' src={item.img} alt={item.title} /></div>
+              <h2 className=' max-w-[327px] font-medium text-[28px] leading-[34px] mt-3'>{item.title}</h2>
+              <p className='max-w-[327px] font-normal leading-[25px] text-gray-400 text-wrap mt-3'>{item.description}</p>
+              <button className=' mt-3 h-[40px] px-3 rounded-[7px] bg-[#2AB6911C]'>{item.btn}</button>
+            </div>
+          ))
+         }
+        </div>
+      </div>
+
+    </div>
   )
 }
 
---
+++ vite-project/src/data/blog/Blogdata.js
@@ -0,0 +1,27 @@
+let Blogdata = [
+    {
+        id: 1,
+        img :'vase-1-b 2.png',
+        title:'Make myspace your best designed space',
+        description:'A lot of different components that will help you create the perfect look for your project',
+        btn: 'Design'
+
+    },
+    {
+        id: 1,
+        img :'vase-1-b 2 (2).png',
+        title:'My company culture has changed today',
+        description:'A lot of different components that will help you create the perfect look for your project',
+        btn: '3D illustrations'
+
+    },
+    {
+        id: 1,
+        img :'vase-1-b 2 (1).png',
+        title:'Professionals in craft! All products were super great',
+        description:'A lot of different components that will help you create the perfect look for your project',
+        btn: 'Development'
+
+    },
+]
+export default Blogdata;
