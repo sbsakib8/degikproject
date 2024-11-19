787aedb8d229c431083b63ae205a3a24927dff16
sakib <sbsakibsarkar8@gmail.com>
2024-11-19 18:23:51 +0600

    footer data

---
+++ vite-project/src/data/footer/Footerdata.js
@@ -0,0 +1,34 @@
+let footerdata = [
+    {
+        id: 1,
+        title: 'Home',
+        link1: 'Become Affiliate',
+        link2: 'Go Unlimited',
+        link3: 'Services'
+    },
+    {
+        id: 2,
+        title: 'About Us',
+        link1: '/about',
+        link2: '/abouts',
+        link3: 'Become Affiliate',
+        link4: 'Go Unlimited',
+        link5: 'Services'
+    },
+    {
+        id: 3,
+        title: 'About Us',
+        link1: 'License',
+        link2: 'PolicRefundy',
+        link3: 'About Us',
+        link5: 'Become Affiliate',
+        
+    },
+    {
+        id: 4,
+        title: 'About Us',
+        link1: 'Contacts',
+        link2: 'PolicRefundy',
+        link3: 'PolicRefundy'
+    }
+]
